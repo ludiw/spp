6585a9a1ed89d41014a18bac330beca1eb3b491d
ludiwidiyawan <ludiwidiyawan@gmail.com>
2022-02-02 12:21:50 +0700

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\NotificationController;
 
 Route::middleware(['auth', 'verified', 'installed'])->group(function () {
     Route::get('notifications', NotificationController::class)->name('notifications');
-});
+}); 
